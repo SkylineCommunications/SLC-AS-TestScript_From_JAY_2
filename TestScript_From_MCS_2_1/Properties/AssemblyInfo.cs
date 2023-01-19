5eb2ea2b964154ef8942baeb4a548dee52a359ae
MichielCS <michiel.clepkens@skyline.be>
2023-01-19 13:23:24 +0100

    Added the TestScript_From_MCS_2 script.

---
+++ TestScript_From_MCS_2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestScript_From_MCS_2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestScript_From_MCS_2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7A354583-7237-4F4B-BFF3-37696AC4ADAF")]
+[assembly: AssemblyVersion("1.0.0.0")]
